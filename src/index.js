b7b507c99853dfb13144322ac1919706e9ce7437
harshbix <kidabixson@gmail.com>
2023-08-23 23:58:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import View from './components/view';
 import Other from './components/other';
 
-
-
-// Define a functional component
 function App() {
-    return (
-        <div className='row'>
-            <div className='col-3'>
-                <View />
-            </div>
-            <div className='col-9'>
-                <Other />
-            </div>
-        </div>
-    );
+  return (
+    <div className='row'>
+      <div className='col-3'>
+        <View />
+      </div>
+      <div className='col-9'>
+        <Other />
+      </div>
+    </div>
+  );
 }
 
-// Render the App component into the root element
 ReactDOM.render(<App />, document.getElementById('root'));

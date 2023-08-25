0531627bfa671edda8dcd6db4e876de26fe12a60
harshbix <kidabixson@gmail.com>
2023-08-25 21:19:44 +0300

    added background for image section

--- src/components/view.jsx
+++ src/components/view.jsx
@@ -1,10 +1,17 @@
 const view = () => {
-    return ( 
-
-        <div className="box">
-            <h1>Hello Mom!</h1>
+  return (
+    <div className="box">
+      <div className="image">
+        <div className="card-front">
+          <h1>
+            hey Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ut
+            eum sit delectus facere cum repudiandae iure? Iure dicta tenetur,
+            odio fugit nisi vel ab voluptas animi quaerat! Saepe, ratione.
+          </h1>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default view;

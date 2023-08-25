8ce922ae5740c1cc4b6cee2a14803d9200b83689
harshbix <kidabixson@gmail.com>
2023-08-25 14:04:07 +0300

    Update other.jsx

--- src/components/other.jsx
+++ src/components/other.jsx
@@ -2,59 +2,64 @@ import React from 'react';
 
 const CreditCardForm = () => {
   return (
-    <div className='box'>
-      <form>
-        <div>
-          <label htmlFor="cardHolderName">CARDHOLDER NAME</label>
+    <div className='container d-flex justify-content-center align-items-center' style={{ minHeight: '100vh' }}>
+      <form className='card p-4 w-100' style={{ maxWidth: '400px', borderRadius: '0' }}>
+        <div className='mb-3'>
+          <label htmlFor='cardHolderName' className='form-label'>CARDHOLDER NAME</label>
           <input
-            type="text"
-            id="cardHolderName"
-            name="cardHolderName"
-            placeholder="e.g Jane Appleseed"
+            type='text'
+            id='cardHolderName'
+            name='cardHolderName'
+            className='form-control'
+            placeholder='e.g Jane Appleseed'
             required
           />
         </div>
-        <div>
-          <label htmlFor="cardNumber">CARD NUMBER</label>
+        <div className='mb-3'>
+          <label htmlFor='cardNumber' className='form-label'>CARD NUMBER</label>
           <input
-            type="text"
-            id="cardNumber"
-            name="cardNumber"
-            placeholder="e.g 1234 5678 9012 3456"
-            pattern="[0-9 ]{13,19}"
+            type='text'
+            id='cardNumber'
+            name='cardNumber'
+            className='form-control'
+            placeholder='e.g 1234 5678 9012 3456'
+            pattern='[0-9 ]{13,19}'
             required
           />
         </div>
-        <div>
-          <label htmlFor="expDate">Exp. Date (MM / YY)</label>
-          <div>
+        <div className='mb-3'>
+          <label htmlFor='expDate' className='form-label'>Exp. Date (MM / YY)</label>
+          <div className='d-flex'>
             <input
-              type="text"
-              id="expMonth"
-              name="expMonth"
-              placeholder="MM"
-              pattern="(0[1-9]|1[0-2])"
+              type='text'
+              id='expMonth'
+              name='expMonth'
+              className='form-control me-2'
+              placeholder='MM'
+              pattern='(0[1-9]|1[0-2])'
               required
             />
             <input
-              type="text"
-              id="expYear"
-              name="expYear"
-              placeholder="YY"
-              pattern="[0-9]{2}"
+              type='text'
+              id='expYear'
+              name='expYear'
+              className='form-control me-2'
+              placeholder='YY'
+              pattern='[0-9]{2}'
               required
             />
             <input
-              type="text"
-              id="cvv"
-              name="cvv"
-              placeholder="CVV"
-              pattern="[0-9]{3}"
+              type='text'
+              id='cvv'
+              name='cvv'
+              className='form-control'
+              placeholder='CVV'
+              pattern='[0-9]{3}'
               required
             />
           </div>
         </div>
-        <button type="submit">Submit</button>
+        <button type='submit' className='btn btn-primary w-100'>Submit</button>
       </form>
     </div>
   );

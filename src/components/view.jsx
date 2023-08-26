cd75f07c08cbd2dfe6ead842eb339c9b1ee169d7
harshbix <kidabixson@gmail.com>
2023-08-26 13:44:44 +0300

    THE REDO

--- src/components/view.jsx
+++ src/components/view.jsx
@@ -2,31 +2,21 @@ import React from "react";
 
 const View = () => {
   return (
-    <div
-      className="box"
-      style={{ backgroundImage: `url("/images/bg-main-desktop.png")` }}
-    >
-      <div className="image">
-        <img
-          src="/images/bg-card-front.png"
-          alt="Card Front"
-          className="card-front"
-        />
+    <div className="box">
+      {/* <div className="image position-relative">
+        <img src="/images/bg-card-front.png" alt="Card Front" className="card-front" />
         <img src="/images/card-logo.svg" alt="" className="circle" />
-        <div className="details text-white">
+        <div className="details text-white position-absolute">
           <h3>0000 0000 0000 0000</h3>
-          <div className="nameAndDate row">
-            <p className="fname col-10">Jane Appleseed</p>
-            <p className="expDate col-2">09/23</p>
+          <div className="nameAndDate">
+            <p className="name">Jane Appleseed</p>
+            <p className="expDate">00/00</p>
           </div>
         </div>
-        <img
-          src="/images/bg-card-back.png"
-          alt="Card Front"
-          className="card-back"
-        />
+        <img src="/images/bg-card-back.png" alt="Card Back" className="card-back" />
         <div className="cvv">000</div>
-      </div>
+      </div> */}
+      <h1>HEY THERE!</h1>
     </div>
   );
 };

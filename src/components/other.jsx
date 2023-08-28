0160efc30d0734f7b89410c45b66c34eab990bf2
harshbix <kidabixson@gmail.com>
2023-08-29 02:45:29 +0300

    update

--- src/components/other.jsx
+++ src/components/other.jsx
@@ -1,67 +1,87 @@
-import React from 'react';
+import React from "react";
 
 const CreditCardForm = () => {
   return (
-    <div className='container d-flex justify-content-center align-items-center CreditCardForm' style={{ minHeight: '100vh' }}>
-      <form className='card p-4 border-0'  style={{ maxWidth: '400px' }}>
-        <div className='mb-3'>
-          <label htmlFor='cardHolderName' className='form-label'>CARDHOLDER NAME</label>
-          <input
-            type='text'
-            id='cardHolderName'
-            name='cardHolderName'
-            className='form-control'
-            placeholder='e.g Jane Appleseed'
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='cardNumber' className='form-label'>CARD NUMBER</label>
-          <input
-            type='text'
-            id='cardNumber'
-            name='cardNumber'
-            className='form-control'
-            placeholder='e.g 1234 5678 9012 3456'
-            pattern='[0-9 ]{13,19}'
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='expDate' className='form-label'>Exp. Date (MM / YY)</label>
-          <div className='d-flex'>
+    <>
+      <div
+        className="container d-flex justify-content-center align-items-center CreditCardForm"
+        style={{ minHeight: "100vh" }}
+      >
+        <form className="card p-4 border-0" style={{ maxWidth: "400px" }}>
+          <div className="mb-3">
+            <label htmlFor="cardHolderName" className="form-label">
+              CARDHOLDER NAME
+            </label>
             <input
-              type='text'
-              id='expMonth'
-              name='expMonth'
-              className='form-control me-2'
-              placeholder='MM'
-              pattern='(0[1-9]|1[0-2])'
-              required
-            />
-            <input
-              type='text'
-              id='expYear'
-              name='expYear'
-              className='form-control me-2'
-              placeholder='YY'
-              pattern='[0-9]{2}'
-              required
-            />
-            <input
-              type='text'
-              id='cvv'
-              name='cvv'
-              className='form-control'
-              placeholder='CVV'
-              pattern='[0-9]{3}'
+              type="text"
+              id="cardHolderName"
+              name="cardHolderName"
+              className="form-control"
+              placeholder="e.g Jane Appleseed"
               required
             />
           </div>
-        </div>
-        <button type='submit' className='btn btn-lg w-100 dark'>Comfirm</button>
-      </form>
-    </div>
+          <div className="mb-3">
+            <label htmlFor="cardNumber" className="form-label">
+              CARD NUMBER
+            </label>
+            <input
+              type="text"
+              id="cardNumber"
+              name="cardNumber"
+              className="form-control"
+              placeholder="e.g 1234 5678 9012 3456"
+              pattern="[0-9 ]{13,19}"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="expDate" className="form-label">
+              Exp. Date (MM / YY)
+            </label>
+            <div className="d-flex">
+              <input
+                type="text"
+                id="expMonth"
+                name="expMonth"
+                className="form-control me-2"
+                placeholder="MM"
+                pattern="(0[1-9]|1[0-2])"
+                required
+              />
+              <input
+                type="text"
+                id="expYear"
+                name="expYear"
+                className="form-control me-2"
+                placeholder="YY"
+                pattern="[0-9]{2}"
+                required
+              />
+              <input
+                type="text"
+                id="cvv"
+                name="cvv"
+                className="form-control"
+                placeholder="CVV"
+                pattern="[0-9]{3}"
+                required
+              />
+            </div>
+          </div>
+          <button type="submit" className="btn btn-lg w-100 dark">
+            Comfirm
+          </button>
+        </form>
+      </div>
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/harshbix">JuniorJeconia</a>.
+      </div>
+    </>
   );
 };
 

d3740a5e511db528d3fc27e5c4cf6714f0c6a0f6
harshbix <kidabixson@gmail.com>
2023-08-28 17:45:43 +0300

    update card

--- src/components/frontCard.jsx
+++ src/components/frontCard.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
+
 const frontCard = () => {
     return ( 
         <div className="card-container">
-            <div className="card-logo p-3">
+            <div className="card-logo p-4">
                 <img src="/images/card-logo.svg" />
+                <div className="details mt-5">
+                    <h4 className='text-start text-white my-1'>1234 5678 9012 3456</h4>
+                    <div className="nameAndDate d-flex justify-content-between text-white my-2">
+                        <p className='ms-1'>JANE APPLESEED</p>
+                        <p className='me-4'>09/23</p>
+                    </div>
+                </div>
             </div>
         </div>
      );

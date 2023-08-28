b6cd7088ad7084fac9054fd94211d8d1a79a2c55
harshbix <kidabixson@gmail.com>
2023-08-28 17:19:29 +0300

    added logo

--- src/components/frontCard.jsx
+++ src/components/frontCard.jsx
@@ -1,8 +1,9 @@
 const frontCard = () => {
     return ( 
         <div className="card-container">
-
-
+            <div className="card-logo p-3">
+                <img src="/images/card-logo.svg" />
+            </div>
         </div>
      );
 }

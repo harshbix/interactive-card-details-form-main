8288ea8018166fa40b7a41f5b096b78793c2aab3
harshbix <kidabixson@gmail.com>
2023-08-28 18:08:44 +0300

    update back card

--- src/components/backCard.jsx
+++ src/components/backCard.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
+
+
 const backCard = () => {
     return ( 
         <div className="card-container-back">
-
-            
+            <div className="cvv"><h6 className='text-white'>123</h6></div>
         </div>
      );
 }
